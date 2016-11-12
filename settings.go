63c9981b7b471b6eeeac045835f45d8e5dda559a
Lennart Espe <lennart@mooxmirror.io>
2016-11-12 22:27:44 +0100

    add name settings

--- settings.go
+++ settings.go
@@ -5,6 +5,33 @@ import (
 	"net/http"
 )
 
+func settingsNameHandler(w http.ResponseWriter, r *http.Request) {
+	accessTokens, ok := r.URL.Query()["token"]
+	if !ok || len(accessTokens) != 1 || validate(accessTokens[0]) == nil {
+		http.Error(w, "invalid access token", http.StatusUnauthorized)
+		return
+	}
+
+	names, ok := r.URL.Query()["name"]
+	if !ok || len(names) != 1 {
+		http.Error(w, "invalid color", http.StatusBadRequest)
+		return
+	}
+	name := names[0]
+	id := getID(accessTokens[0])
+
+	var profile Profile
+	database.First(&profile, "ID = ?", id)
+	database.Model(&profile).Update(Profile{Name: name})
+
+	data, err := json.Marshal(toJSONProfile(profile))
+	if err != nil {
+		http.Error(w, "json error", http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
+
 func settingsColorHandler(w http.ResponseWriter, r *http.Request) {
 	accessTokens, ok := r.URL.Query()["token"]
 	if !ok || len(accessTokens) != 1 || validate(accessTokens[0]) == nil {
