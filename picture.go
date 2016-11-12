6793397789f7dd32b3374e027e1b1897697db792
Lennart Espe <lennart@mooxmirror.io>
2016-11-12 22:22:39 +0100

    add working picture uploader

--- picture.go
+++ picture.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 const PICTURE_FOLDER = "picture/"
@@ -25,6 +27,32 @@ func addPicture(id uint, stream io.Reader) (string, error) {
 	}
 
 	return path, nil
+}
+
+// Handle /picture GET
+func getPictureHandler(w http.ResponseWriter, r *http.Request) {
+	accessTokens, ok := r.URL.Query()["token"]
+	if !ok || len(accessTokens) != 1 || validate(accessTokens[0]) == nil {
+		http.Error(w, "invalid access token", http.StatusUnauthorized)
+		return
+	}
+
+	vars := mux.Vars(r)
+	id, err := strconv.ParseUint(vars["id"], 10, 64)
+
+	path := PICTURE_FOLDER + strconv.FormatUint(id, 10)
+	file, err := os.Open(path)
+	if err != nil {
+		http.Error(w, "image not found", http.StatusNotFound)
+		return
+	}
+	defer file.Close()
+
+	_, err = io.Copy(w, file)
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
 }
 
 // Handle /picture POST
