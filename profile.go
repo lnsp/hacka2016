6f244df2a28851c62f110ad7fe5117ad5166d251
Lennart Espe <lennart@mooxmirror.io>
2016-11-12 18:28:49 +0100

    split settings from profile

--- profile.go
+++ profile.go
@@ -244,30 +244,3 @@ func meetHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Write(data)
 }
-
-func settingsColorHandler(w http.ResponseWriter, r *http.Request) {
-	accessTokens, ok := r.URL.Query()["token"]
-	if !ok || len(accessTokens) != 1 || validate(accessTokens[0]) == nil {
-		http.Error(w, "invalid access token", http.StatusUnauthorized)
-		return
-	}
-
-	colors, ok := r.URL.Query()["color"]
-	if !ok || len(colors) != 1 {
-		http.Error(w, "invalid color", http.StatusBadRequest)
-		return
-	}
-	rgb := colors[0]
-	id := getID(accessTokens[0])
-
-	var profile Profile
-	database.First(&profile, "ID = ?", id)
-	database.Model(&profile).Update(Profile{Color: rgb})
-
-	data, err := json.Marshal(toJSONProfile(profile))
-	if err != nil {
-		http.Error(w, "json error", http.StatusInternalServerError)
-		return
-	}
-	w.Write(data)
-}
